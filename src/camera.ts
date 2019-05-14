7f22cd089b2cd5145861d27bcfca1fb4310b1992
TrySpace <mgasman@gmail.com>
2019-05-14 12:02:29 +0200

    era

--- src/camera.ts
+++ src/camera.ts
@@ -2,12 +2,12 @@ import * as BABYLON from 'babylonjs';
 import { SceneInstance } from './SceneInstance';
 
 export class Camera {
-  private _camera: BABYLON.ArcRotateCamera;
+  private camera: BABYLON.ArcRotateCamera;
   public sceneInstance: SceneInstance;
 
   constructor(sceneInstance: SceneInstance, nr: number) {
     this.sceneInstance = sceneInstance;
-    this._camera = new BABYLON.ArcRotateCamera(`Camera-${nr}`, 3 * Math.PI / 2, Math.PI / 2.5, 30, BABYLON.Vector3.Zero(), this.sceneInstance.scene);
-    this._camera.attachControl(this.sceneInstance.renderCanvas.canvas, true);
+    this.camera = new BABYLON.ArcRotateCamera(`Camera-${nr}`, 3 * Math.PI / 2, Math.PI / 2.5, 30, BABYLON.Vector3.Zero(), this.sceneInstance.scene);
+    this.camera.attachControl(this.sceneInstance.renderCanvas.canvas, true);
   }
 }
